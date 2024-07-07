22fa3c00f1627fdb680cd29a6466afb450ef11dc
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2024-07-08 02:52:27 +0400

    before fixing scroll

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(ScrollTrigger)
+
 const halfCircles = document.querySelectorAll(".half-circle")
 const halfCircleTop = document.querySelector(".half-circle-top")
 const progressBarCircle = document.querySelector(".progressbar-circle")
@@ -12,24 +14,292 @@ const circleSeven = document.querySelector(".s-circle-7")
 const halfTwo = document.querySelector(".half-two")
 const halfOne = document.querySelector(".half-one")
 const mainTitle = document.querySelector(".main-title")
+const slideOne = document.querySelector(".slide-1")
 
+const pageViewportHeight = window.innerHeight
+const pageHeight = document.documentElement.scrollHeight * 7
+// ------------------------------slides------------------------------
+
+// ------------------------slide one-------------
+let slideOneImgOneTween = gsap.fromTo(
+  ".slide-1-img-1",
+  {
+    y: 500,
+    opacity: 0,
+    // duration: 3,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 0.5,
+  }
+)
+
+let slideOneImgTwoTween = gsap.fromTo(
+  ".slide-1-img-2",
+  {
+    y: 500,
+    opacity: 0,
+    // duration: 3,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 0.5,
+  }
+)
+// ------------------------slide two-------------
+let slideTwoImgOneTween = gsap.fromTo(
+  ".slide-2-img-1",
+  {
+    y: 100,
+    opacity: 0,
+    paused: true,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 0.5,
+    paused: true,
+  }
+)
+
+let slideTwoImgTwoTween = gsap.fromTo(
+  ".slide-2-img-2",
+  {
+    y: -100,
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 0.5,
+    paused: true,
+  }
+)
+// ------------------------slide three-------------
+let slideThreeImgOneTween = gsap.fromTo(
+  ".slide-3-img-1",
+  {
+    y: 100,
+    opacity: 0,
+    paused: true,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 0.5,
+    paused: true,
+  }
+)
+
+let slideThreeImgTwoTween = gsap.fromTo(
+  ".slide-3-img-2",
+  {
+    y: -100,
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 0.5,
+    paused: true,
+  }
+)
+// ------------------------slide four-------------
+let slideFourImgOneTween = gsap.fromTo(
+  ".slide-4-img-1",
+  {
+    x: -100,
+    opacity: 0,
+    paused: true,
+  },
+  {
+    x: 0,
+    opacity: 1,
+    duration: 0.5,
+    paused: true,
+  }
+)
+
+let slideFourImgTwoTween = gsap.fromTo(
+  ".slide-4-img-2",
+  {
+    y: 100,
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 0.5,
+    paused: true,
+  }
+)
+let slideFourImgThreeTween = gsap.fromTo(
+  ".slide-4-img-3",
+  {
+    y: -100,
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 0.5,
+    paused: true,
+  }
+)
+let slideFourImgFourTween = gsap.fromTo(
+  ".slide-4-img-4",
+  {
+    y: -180,
+    opacity: 0,
+  },
+  {
+    y: -130,
+    opacity: 1,
+    duration: 0.5,
+    paused: true,
+  }
+)
+let slideFourImgFiveTween = gsap.fromTo(
+  ".slide-4-img-5",
+  {
+    y: 100,
+    x: 100,
+    opacity: 0,
+  },
+  {
+    y: 0,
+    x: 0,
+
+    opacity: 1,
+    duration: 0.5,
+    paused: true,
+  }
+)
+// ------------------------slide five-------------
+let slideFiveImgOneTween = gsap.fromTo(
+  ".slide-5-img-1",
+  {
+    y: -180,
+    opacity: 0,
+    paused: true,
+  },
+  {
+    y: -120,
+    opacity: 1,
+    duration: 0.5,
+    paused: true,
+  }
+)
+
+let slideFiveImgTwoTween = gsap.fromTo(
+  ".slide-5-img-2",
+  {
+    y: 110,
+    opacity: 0,
+  },
+  {
+    y: 100,
+    opacity: 1,
+    duration: 0.5,
+    paused: true,
+  }
+)
+let slideFiveImgThreeTween = gsap.fromTo(
+  ".slide-5-img-3",
+  {
+    y: -180,
+    opacity: 0,
+  },
+  {
+    y: -25,
+    opacity: 1,
+    duration: 0.5,
+    paused: true,
+  }
+)
+let slideFiveImgFourTween = gsap.fromTo(
+  ".slide-5-img-4",
+  {
+    y: 210,
+    opacity: 0,
+  },
+  {
+    y: 200,
+    opacity: 1,
+    duration: 0.5,
+    paused: true,
+  }
+)
+let slideFiveImgFiveTween = gsap.fromTo(
+  ".slide-5-img-5",
+  {
+    x: 100,
+    opacity: 0,
+  },
+  {
+    x: 0,
+
+    opacity: 1,
+    duration: 0.5,
+    paused: true,
+  }
+)
+// ------------------------slide six-------------
+let slideSixImgOneTween = gsap.fromTo(
+  ".slide-6-img-1",
+  {
+    y: 100,
+    opacity: 0,
+    paused: true,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 0.2,
+    paused: true,
+  }
+)
+
+// ------------------------slide seven-------------
+let slideSevenImgOneTween = gsap.fromTo(
+  ".slide-7-img-1",
+  {
+    y: 200,
+    opacity: 0,
+    paused: true,
+  },
+  {
+    y: 180,
+    opacity: 1,
+    duration: 0.5,
+    paused: true,
+  }
+)
+let slideSevenImgTwoTween = gsap.fromTo(
+  ".slide-7-img-2",
+  {
+    y: 200,
+    opacity: 0,
+    paused: true,
+  },
+  {
+    y: 180,
+    opacity: 1,
+    duration: 0.5,
+    paused: true,
+  }
+)
+
+// -----------------------------------------
 halfTwo.addEventListener("scroll", () => {
-  gsap.registerPlugin(ScrollTrigger)
-
-  const pageViewportHeight = window.innerHeight
-  const pageHeight = document.documentElement.scrollHeight * 7
   const scrolledPortion = halfTwo.scrollTop
-
-  console.log(pageViewportHeight)
-  console.log(pageHeight)
-  console.log("you scroled", scrolledPortion)
 
   const scrolledPortionDegree =
     (scrolledPortion / (pageHeight - pageViewportHeight)) * 271
 
-  console.log("scrolledPortionDegree: ", scrolledPortionDegree)
   const scrolledPortionPercent = Math.floor((scrolledPortionDegree / 360) * 100)
-  console.log("scrolledPortionPercent: ", scrolledPortionPercent)
 
   progressBarCircle.textContent = `${scrolledPortionPercent}%`
 
@@ -43,6 +313,79 @@ halfTwo.addEventListener("scroll", () => {
       halfCircleTop.style.opacity = "1"
     }
   })
+  // ----------------------------gsap conditions-----------------------
+  if (scrolledPortionPercent <= 3) {
+    slideOneImgOneTween.play()
+    slideOneImgTwoTween.play()
+    slideTwoImgOneTween.reverse()
+    slideTwoImgTwoTween.reverse()
+    slideThreeImgOneTween.reverse()
+    slideThreeImgTwoTween.reverse()
+    slideFourImgOneTween.reverse()
+    slideFourImgTwoTween.reverse()
+    slideFourImgThreeTween.reverse()
+    slideFourImgFourTween.reverse()
+    slideFourImgFiveTween.reverse()
+    slideFiveImgOneTween.reverse()
+    slideFiveImgTwoTween.reverse()
+    slideFiveImgThreeTween.reverse()
+    slideFiveImgFourTween.reverse()
+    slideFiveImgFiveTween.reverse()
+  } else if (scrolledPortionPercent < 11) {
+    slideOneImgOneTween.reverse()
+    slideOneImgTwoTween.reverse()
+  } else if (scrolledPortionPercent === 12) {
+    slideTwoImgOneTween.play()
+    slideTwoImgTwoTween.play()
+  } else if (scrolledPortionPercent == 15) {
+    slideTwoImgOneTween.reverse()
+    slideTwoImgTwoTween.reverse()
+  } else if (scrolledPortionPercent < 25) {
+    slideThreeImgOneTween.reverse()
+    slideThreeImgTwoTween.reverse()
+  } else if (scrolledPortionPercent == 25) {
+    slideThreeImgOneTween.play()
+    slideThreeImgTwoTween.play()
+  } else if (scrolledPortionPercent <= 27) {
+    slideThreeImgOneTween.reverse()
+    slideThreeImgTwoTween.reverse()
+  } else if (scrolledPortionPercent < 37) {
+    slideFourImgOneTween.reverse()
+    slideFourImgTwoTween.reverse()
+    slideFourImgThreeTween.reverse()
+    slideFourImgFourTween.reverse()
+    slideFourImgFiveTween.reverse()
+  } else if (scrolledPortionPercent == 37) {
+    slideFourImgOneTween.play()
+    slideFourImgTwoTween.play()
+    slideFourImgThreeTween.play()
+    slideFourImgFourTween.play()
+    slideFourImgFiveTween.play()
+  } else if (scrolledPortionPercent <= 40) {
+    slideFourImgOneTween.reverse()
+    slideFourImgTwoTween.reverse()
+    slideFourImgThreeTween.reverse()
+    slideFourImgFourTween.reverse()
+    slideFourImgFiveTween.reverse()
+  } else if (scrolledPortionPercent == 49) {
+    slideFiveImgOneTween.play()
+    slideFiveImgTwoTween.play()
+    slideFiveImgThreeTween.play()
+    slideFiveImgFourTween.play()
+    slideFiveImgFiveTween.play()
+  } else if (scrolledPortionPercent < 62) {
+    slideSixImgOneTween.reverse()
+  } else if (scrolledPortionPercent == 62) {
+    slideSixImgOneTween.play()
+  } else if (scrolledPortionPercent == 65) {
+    slideSixImgOneTween.reverse()
+  } else if (scrolledPortionPercent == 75) {
+    slideSevenImgOneTween.play()
+    slideSevenImgTwoTween.play()
+  }
+
+  // -----------------------------condition end-------------------------
+
   if (scrolledPortionPercent < 12) {
     circleTwo.style.backgroundColor = "#91c0ff"
     circleThree.style.backgroundColor = "#91c0ff"
@@ -56,6 +399,10 @@ halfTwo.addEventListener("scroll", () => {
     mainTitle.style.backgroundColor = "#551196"
     halfTwo.style.backgroundColor = "#c6c6c8"
     halfTwo.style.backgroundImage = "none"
+    // slideOneImgOneTween.play()
+    // slideOneImgTwoTween.play()
+    // slideTwoImgOneTween.reverse()
+    // slideTwoImgTwoTween.reverse()
   } else if (scrolledPortionPercent >= 12 && scrolledPortionPercent < 25) {
     circleTwo.style.backgroundColor = "#0189ff"
     circleThree.style.backgroundColor = "#91c0ff"
@@ -69,6 +416,10 @@ halfTwo.addEventListener("scroll", () => {
     mainTitle.style.backgroundColor = "#4826c5"
     halfTwo.style.backgroundColor = "#1c0362"
     halfTwo.style.backgroundImage = "none"
+    // slideOneImgOneTween.reverse()
+    // slideOneImgTwoTween.reverse()
+    // slideTwoImgOneTween.play()
+    // slideTwoImgTwoTween.play()
   } else if (scrolledPortionPercent >= 25 && scrolledPortionPercent < 37) {
     circleTwo.style.backgroundColor = "#0189ff"
     circleThree.style.backgroundColor = "#0189ff"
